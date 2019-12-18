2182d591ce69fcc1be6940c735a2ee828c362efe
simon-towler <simon.towler@icloud.com>
2019-12-18 14:21:14 +0100

    Added a FileHandler to the root logger to write to tmp/wombat.log. In the logging namespace, set the level of com.wombat to FINEST.

--- com/wombat/Nose.java
+++ com/wombat/Nose.java
@@ -1,13 +1,24 @@
 //https://docs.oracle.com/en/java/javase/12/core/java-logging-overview.html
 package com.wombat;
 import java.util.logging.*;
+import java.io.IOException;
 
 public class Nose {
   // Obtain a suitable logger.
   private static Logger logger = Logger.getLogger("com.wombat.nose");
   private static Wombat wombat = new Wombat();
 
-  public static void main(String argv[]) {
+  public static void main(String argv[]) throws IOException {
+
+      Handler fh = new FileHandler("%t/wombat.log");
+      Logger.getLogger("").addHandler(fh);//get the root logger and add the FileHandler to it
+      Logger.getLogger("com.wombat").setLevel(Level.FINEST);
+      /*
+      The console logging below remains unchanged by the above three lines.
+      What is changed is that an XML log file is now written to temp, which
+      includes all the original logs to console, but now also the FINE level logs.
+      */
+
     // Log a FINE tracing message
     logger.fine("doing stuff");//doesn't get output, by default
     try {
